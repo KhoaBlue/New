2b26caff4ec5b3bfc9806412b6a49dd0719175dc
Noridom1 <thinhphuc2005@gmail.com>
2024-03-23 09:27:11 +0700

    Fixing functions 2

--- ManagementSystem/ManagementSystem/ManagementSystem.cpp
+++ ManagementSystem/ManagementSystem/ManagementSystem.cpp
@@ -1,5 +1,15 @@
 #include "Function.h"
 
 int main() {
+	SchoolYear *SyList;
+	User *StaffList;
+	Student *StudentList;
+	int numSY;
+	loadSchoolYearList(SyList, numSY);
+	for (int i = 0; i < numSY; ++i) {
+		cout << SyList[i].Name << endl;
+	}
+	delete[] StaffList;
+	delete[] SyList;
 	return 0;
 }
